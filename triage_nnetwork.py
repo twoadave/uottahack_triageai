f37c6367d3f1ccf1f0a2e91265fe0305aa140e91
twoadave <gayowsky.d@gmail.com>
2024-03-02 13:37:27 -0500

    added generating function for symptoms

--- triage_nnetwork.py
+++ triage_nnetwork.py
@@ -2,3 +2,57 @@
 Neural network triaging.
 
 Created by: David J. Gayowsky, March 2nd 2024'''
+
+#######################################################################
+
+import numpy as np
+import itertools
+
+#######################################################################
+
+'''Create test case criteria: 
+    Heart attack,
+    Paper cut.
+    Where each scenario has ans = 10 yes or no questions, e.g. becomes an array of 10 binary values.
+    2^10 possible scenarios per situation!
+
+Create test case data sets:
+    Take all combinations without repetition of criteria, and calculate corresponding risk_categories = 3 for risk level (0, 1, 2).
+    Compute the amount of each risk level data present (e.g. number of data points corresponding to each category).
+    Split N data points into training and testing data sets, where the test data set consists of M total data points, where we make sure to sample from smaller parts of the distribution e.g. edge cases. Such that num_risk_i ≈ num_risk_j = M/risk_categories for all i ≠ j wherever possible.
+    Note: is there a way to do this where we can expand to risk_categories cases? Look up efficient data sampling? 
+
+Train and test neural network using subsets of training data of order P = Q/M ≤ M, measuring speed and accuracy each time, finding a balance between accuracy and speed. 
+    More training data = more accuracy, but lower speed!
+    Use timer to measure how much time each run takes.
+    Additionally, look at effect of number of nodes/layers on speed and accuracy.
+    Find order of operations?
+    Note: is there an ideal level of neural network accuracy? Say, for healthcare based applications, do we want our neural network to be accurate 99.5% of the time, or is 95% sufficient?
+
+Save neural network weights with desired accuracy.
+
+Test again using saved neural network weights, loading from file and measuring time.
+
+Demo test cases.
+'''
+
+#######################################################################
+
+#Define a function to create a sample answerspace:
+def create_sample_answerspace(num_ans, condition):
+
+    #Create array of possible answer spaces:
+    poss_ans_combinations = []
+
+    #Create all possible lists of combinations without repetition that are N in length of 1 or 0:
+    poss_comb = list(itertools.product(range(2), repeat=num_ans))
+    #Convert to array for use in neural network.
+    poss_comb = np.array(poss_comb)
+
+    return poss_comb
+
+#######################################################################
+    
+if __name__ == '__main__':
+
+    create_sample_answerspace(10, condition='heart attack')
