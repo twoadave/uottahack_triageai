63a0608f1c0f917f1b24a45df61b0c58ad287218
twoadave <gayowsky.d@gmail.com>
2024-03-02 17:49:46 -0500

    imports wwwww i have no idea what im doing i hope whoever is reading this is having fun !

--- triage_neuralnet.py
+++ triage_neuralnet.py
@@ -6,3 +6,9 @@ Created by: David J. Gayowsky, March 2nd 2024
 IN THIS FILE: Actual neural network code, creation and training of neural network, and saving weights for future use.'''
 
 #######################################################################
+
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+#######################################################################
